337c37b4d5c4205d4c4a5e6697c30f18de840c23
hy <13603742597198519>
2021-12-13 16:13:54 +0800

    修改pom配置

--- demo.api/src/main/java/demo/api/controller/WorkWeXinController.java
+++ demo.api/src/main/java/demo/api/controller/WorkWeXinController.java
@@ -4,12 +4,14 @@ import demo.common.work.wexin.FinanceDemo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author ZML
  * @version 1.0
  * @date 2021/12/2 1:22 下午
  */
+@RestController
 @RequestMapping("api/weixin/")
 public class WorkWeXinController {
 
